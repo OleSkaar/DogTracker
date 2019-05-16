8da643ff8808170fc856830ed500b0e5201baca1
Ole Skaar <olejohska@gmail.com>
2019-05-16 16:24:40 +0200

    Andre iterasjon
    Første fungerende prototype. Viser antall repetisjoner av en øvelse, antall fullførte øvelser, og ukentlig progresjon for to dummy-brukere.

--- DogTracker.ino
+++ DogTracker.ino
@@ -1,9 +1,9 @@
 /*
-  DogTracker 0.1 Alpha
+  DogTracker 0.2 Alpha
 
   Dings som kan hjelpe til aa motivere hundeeiere ved aa lagre og vise ukentlig treningsprogresjon.
 
-  Med Piezo, LEDs, og LCD-skjerm.
+  Med LED-ringer, piezo, og potentiometer som kontroller
 
   Guide til LED: 
 
@@ -12,55 +12,100 @@
 */
 
 // Import
-#include <LiquidCrystal.h>
 #include <Adafruit_NeoPixel.h>
 
 // Oppsett av pins
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2, kp = 10, pz = 6, led = 9;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+const int kp = 10, pz = 6, ledStrip = 5, ledRing = 9, potPin = A0;
 
 // Oppsett av LEDs
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(12, led, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel ring = Adafruit_NeoPixel(12, ledRing, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(8, ledStrip, NEO_GRB + NEO_KHZ800);
 
-// datavariabler
+// oppsett
 int repetisjoner = 0;
-int ovelser = 0;
+//int ovelser = 0;
+int ovelserPerDag = 3;
 const int repsPerOvelse = 7;
-int leds[] = {7, 8, 13};
 int antallTrykk = 0;
 int knapp;
 int debounceTid = 50;
 unsigned long sisteDebounce = 0;
 boolean harTrykket = false;
+int startPunktOvelseLED = 11;
+int potVal;
+int angle;
+int aktivOvelse;
+int aktivBruker;
+const int antallOvelser = 3;
+const int antallBrukere = 2;
+
+int brukerOgPoengMatrise[antallBrukere][antallOvelser];
+int aktivOvelsePoeng;
+int ukentligPoeng[antallBrukere];
+
+// datoer
+// Date forrigeDato;
+// Date dagensDato;
+
+// brukervariabler
+int bruker1Poeng;
+int bruker2Poeng;
+
+int bruker1Sitt;
+int bruker1Bli;
+int bruker1Kom;
+
+int bruker2Sitt = 1;
+int bruker2Bli = 2;
+int bruker2Kom = 0;
+
+int sittReps;
+int bliReps;
+int komReps;
 
 void setup() {
-  // Sett opp antall kolonner og rader paa LCD-skjermen:
-  lcd.begin(16, 2);
-  // Skriv til skjermen:
-  lcd.print("Ovelse: Sitt!");
-  for (int i = 0; i < 3; i++) {
-    pinMode(leds[i], OUTPUT);
-  }
-  Serial.begin(9600);
+  // Hent dato fra GPS, sjekker om det er en ny uke, 
+  // hvis ja nullstill, hvis ikke, last inn brukerpoeng for denne uken
+  // Hvis det er en ny dag, nullstill repetisjoner og ovelser, hvis ikke,
+  // hent ukentlig poeng fra minnet
+
+   fyllPoeng();
+   aktivBruker = 0;
+   
+   aktivOvelsePoeng = brukerOgPoengMatrise[aktivBruker][aktivOvelse];
+   oppdaterOvelseLED(aktivOvelsePoeng);
+  
   pinMode(kp, INPUT);
   pinMode(pz, OUTPUT);
   // Sett opp LED. Brightness gaar fra 0 til 255
+  ring.begin();
+  ring.setBrightness(32);
+  ring.show(); // Initialize all pixels to 'off'
   strip.begin();
-  strip.setBrightness(32);
+  strip.setBrightness(255);
   strip.show(); // Initialize all pixels to 'off'
+  Serial.begin(9600);
+  hentUkentligPoeng();
+  oppdaterUkentligLED();
 }
 
 void loop() {
+  lesOvelse();
   if (repetisjoner == repsPerOvelse) {
-    fullfortOvelse();
+    if (aktivOvelsePoeng < ovelserPerDag) {
+      brukerOgPoengMatrise[aktivBruker][aktivOvelse]++;
+      fullfortOvelse();
+    }
     reset();
   }
+  
   knapp = digitalRead(kp);
   if (knapp == 1) {
     if (digitalRead(kp) == 1 && debounce()) {
       if (!harTrykket) {
         antallTrykk += 1;
-          if (repetisjoner < repsPerOvelse) {
+          if (repetisjoner < repsPerOvelse && 
+          brukerOgPoengMatrise[aktivBruker][aktivOvelse] != ovelserPerDag) {
             buttonClick();
           }
         harTrykket = true;
@@ -73,27 +118,31 @@ void loop() {
 }
 
 void buttonClick() {
- repetisjoner++;
- // Lys opp en ny LED
- strip.setPixelColor(repetisjoner - 1, 0, 255, 05);
- strip.show();
+   repetisjoner++;
+   // Lys opp en ny LED
+   ring.setPixelColor(repetisjoner - 1, 0, 255, 05);
+   ring.show();
  }
 
 void reset() {
-    repetisjoner = 0;
-    // Fjern LED lys
-    strip.clear();
-    strip.show();
-    ovelser++;
-    lcd.setCursor(0, 1);
-    lcd.print(ovelser);
-    if (ovelser < 4) {
-      digitalWrite(leds[ovelser-1], HIGH);
-   }
+  repetisjoner = 0;
+  // Fjern LED lys
+  ring.fill(0, 0, repsPerOvelse);
+  ring.show();
 }
 
 boolean debounce() {
   return (millis() - sisteDebounce) >= debounceTid;
+}
+
+int hentAnnenBruker() {
+  int bruker;
+  if (aktivBruker == 1) {
+    bruker = 0;
+  } else {
+    bruker = 1;
+  }
+  return bruker;
 }
 
 void fullfortOvelse() {
@@ -102,4 +151,96 @@ void fullfortOvelse() {
   tone(pz,1319,850);
   delay(800);
   noTone(pz);
+  ring.setPixelColor(startPunktOvelseLED - brukerOgPoengMatrise[aktivBruker][aktivOvelse],255);
+  ukentligPoeng[aktivBruker]++;
+  oppdaterUkentligLED();
+}
+
+void oppdaterOvelseLED(int poeng) {
+  //ring.fill(0, startPunktOvelseLED-2, 3);
+  ring.setPixelColor(10, 0);
+  ring.setPixelColor(9, 0);
+  ring.setPixelColor(8, 0);
+  if (poeng >= 1) {
+    ring.setPixelColor(10, 255);
+  }
+  if (poeng >= 2) {
+    ring.setPixelColor(9, 255);
+  }
+  if (poeng == 3) {
+    ring.setPixelColor(8, 255);
+  }
+  ring.show();
+}
+
+void oppdaterUkentligLED() {
+  int poeng = ukentligPoeng[aktivBruker];
+  int annenBruker = hentAnnenBruker();
+  strip.clear();
+  Serial.println(poeng);
+  for (int i = 0; i < poeng; i++) {
+    int teller = 0;
+    int farge = strip.getPixelColor(teller);
+    if (farge + 42 > 255) {
+      teller++;
+      farge = strip.getPixelColor(teller);
+    }
+    if (ukentligPoeng[aktivBruker] >= ukentligPoeng[annenBruker]) {
+      strip.setPixelColor(teller, 0, 0, farge + 42);
+    } else {
+      strip.setPixelColor(teller, farge + 42, 0, 0);
+    }
+    
+  }
+  strip.show();
+}
+
+void lesOvelse() {
+    potVal = analogRead(potPin);
+    angle = map(potVal, 0, 1023, 0, 179);
+
+  if (aktivOvelse != 0 && angle >= 0 && angle < 60) {
+    
+    Serial.println(angle);
+    Serial.println("kom");
+    
+    aktivOvelse = 0;
+    oppdaterOvelseLED(brukerOgPoengMatrise[aktivBruker][aktivOvelse]);
+  }
+
+  if (aktivOvelse != 1 && angle >= 60 && angle < 120) {
+    
+    Serial.println(angle);
+    Serial.println("sitt");
+    
+    aktivOvelse = 1;
+    oppdaterOvelseLED(brukerOgPoengMatrise[aktivBruker][aktivOvelse]);
+  }
+
+  if (aktivOvelse != 2 && angle >= 120 && angle < 180) {
+    
+    Serial.println(angle);
+    Serial.println("bli");
+    
+    aktivOvelse = 2;
+    oppdaterOvelseLED(brukerOgPoengMatrise[aktivBruker][aktivOvelse]);
+  }
+  delay(50);
+}
+
+void fyllPoeng() {
+  brukerOgPoengMatrise[0][0] = 0;
+  brukerOgPoengMatrise[0][1] = 0;
+  brukerOgPoengMatrise[0][2] = 0;
+  brukerOgPoengMatrise[1][0] = bruker2Kom;
+  brukerOgPoengMatrise[1][1] = bruker2Sitt;
+  brukerOgPoengMatrise[1][2] = bruker2Bli;
+}
+
+void hentUkentligPoeng() {
+  for (int i = 0; i < antallBrukere; i++) {
+    for (int j = 0; j < antallOvelser; j++) {
+      ukentligPoeng[i] += brukerOgPoengMatrise[i][j];
+    }
+  }
 }
