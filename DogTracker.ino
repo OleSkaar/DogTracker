91cb5e459f41f61d6d2c9e63db0af56e3394feb9
Ole Skaar <olejohska@gmail.com>
2019-05-24 19:11:23 +0200

    Added more functionality
    SD card read/write, player 1/2 switch, and cleaned up code

--- DogTracker.ino
+++ DogTracker.ino
@@ -1,5 +1,5 @@
 /*
-  DogTracker 0.2.1  Alpha
+  DogTracker 0.2.2  Alpha
 
   Bruker LED-ringen til å vise om nåværende bruker ligger foran eller bak den andre brukeren.
 
@@ -14,25 +14,35 @@
 
   https://learn.adafruit.com/adafruit-neopixel-uberguide/arduino-library-use
 
+  Kode basert paa SDFat sitt eksempel: 
+
+  https://github.com/greiman/SdFat/blob/master/examples/ReadWrite/ReadWrite.ino
+
 */
 
 // Import
 #include <Adafruit_NeoPixel.h>
+#include <SPI.h>
+#include "SdFat.h"
 
 // Oppsett av pins
-const int kp = 10, pz = 6, ledStrip = 5, ledRing = 9, potPin = A0;
+const int kp = 10, br = 8, pz = 6, ledRing = 9, potPin = A0;
 
 // Oppsett av LEDs
 Adafruit_NeoPixel ring = Adafruit_NeoPixel(12, ledRing, NEO_GRB + NEO_KHZ800);
-//Adafruit_NeoPixel strip = Adafruit_NeoPixel(8, ledStrip, NEO_GRB + NEO_KHZ800);
+uint32_t gul = ring.Color(255, 255, 0);
+uint32_t hvit = ring.Color(255, 255, 255);
+uint32_t blaa = ring.Color(0, 0, 255);
+uint32_t rod = ring.Color(255, 0, 0);
+uint32_t gronn = ring.Color(0, 255, 0);
 
 // oppsett
 int repetisjoner = 0;
-//int ovelser = 0;
 int ovelserPerDag = 3;
-const int repsPerOvelse = 7;
+const int maksmaksRepsPerOvelse = 7;
 int antallTrykk = 0;
 int knapp;
+int bryter;
 int debounceTid = 50;
 unsigned long sisteDebounce = 0;
 boolean harTrykket = false;
@@ -43,14 +53,14 @@ int aktivOvelse;
 int aktivBruker;
 const int antallOvelser = 3;
 const int antallBrukere = 2;
+SdFat SD;
+File dataFil;
+#define SD_CS_PIN SS
 
+int poengPerOvelse[antallBrukere][antallOvelser];
 int brukerOgPoengMatrise[antallBrukere][antallOvelser];
 int aktivOvelsePoeng;
 int ukentligPoeng[antallBrukere];
-
-// datoer
-// Date forrigeDato;
-// Date dagensDato;
 
 // brukervariabler
 int bruker1Poeng;
@@ -60,45 +70,54 @@ int bruker1Sitt;
 int bruker1Bli;
 int bruker1Kom;
 
-int bruker2Sitt = 0;
-int bruker2Bli = 0;
-int bruker2Kom = 0;
-
-int sittReps;
-int bliReps;
-int komReps;
+int bruker2Sitt = 3;
+int bruker2Bli = 1;
+int bruker2Kom = 2;
 
 void setup() {
-  // Hent dato fra GPS, sjekker om det er en ny uke, 
-  // hvis ja nullstill, hvis ikke, last inn brukerpoeng for denne uken
-  // Hvis det er en ny dag, nullstill repetisjoner og ovelser, hvis ikke,
-  // hent ukentlig poeng fra minnet
+  Serial.begin(9600);
 
-   fyllPoeng();
-   aktivBruker = 0;
-   
-   aktivOvelsePoeng = brukerOgPoengMatrise[aktivBruker][aktivOvelse];
-   oppdaterOvelseLED(aktivOvelsePoeng);
+  fyllPoeng();
+  aktivBruker = 1;
+
+  SDKortoppsett();
+  byte test = hentVerdiIFil(aktivBrukerFilnavn());
+  Serial.println(test);
+  aktivOvelsePoeng = brukerOgPoengMatrise[aktivBruker][aktivOvelse];
+  oppdaterOvelseLED(aktivOvelsePoeng);
   
   pinMode(kp, INPUT);
   pinMode(pz, OUTPUT);
   // Sett opp LED. Brightness gaar fra 0 til 255
   ring.begin();
-  ring.setBrightness(32);
-  ring.show(); // Initialize all pixels to 'off'
-  /*
-  strip.begin();
-  strip.setBrightness(255);
-  strip.show(); // Initialize all pixels to 'off'
-  */
-  Serial.begin(9600);
+  ring.setBrightness(100);
+  ring.show();
   hentUkentligPoeng();
   oppdaterUkentligLED();
+  Serial.println(hentVerdiIFil(aktivBrukerFilnavn()));
+  Serial.println("Indeks:");
+  Serial.println(maksRepsPerOvelse
+}
+
+void sjekkAktivBrukerBryter() {
+  bryter = digitalRead(br);
+  if (bryter != aktivBruker) {
+    byttBruker();
+  }
+}
+
+void byttBruker() {
+  if (aktivBruker == 0) {
+    aktivBruker = 1;
+  } else {
+    aktivBruker = 0;
+  }
 }
 
 void loop() {
+  sjekkAktivBrukerBryter();
   lesOvelse();
-  if (repetisjoner == repsPerOvelse) {
+  if (repetisjoner == maksRepsPerOvelse) {
     if (aktivOvelsePoeng < ovelserPerDag) {
       brukerOgPoengMatrise[aktivBruker][aktivOvelse]++;
       fullfortOvelse();
@@ -111,7 +130,7 @@ void loop() {
     if (digitalRead(kp) == 1 && debounce()) {
       if (!harTrykket) {
         antallTrykk += 1;
-          if (repetisjoner < repsPerOvelse && 
+          if (repetisjoner < maksRepsPerOvelse && 
           brukerOgPoengMatrise[aktivBruker][aktivOvelse] != ovelserPerDag) {
             buttonClick();
           }
@@ -127,14 +146,14 @@ void loop() {
 void buttonClick() {
    repetisjoner++;
    // Lys opp en ny LED
-   ring.setPixelColor(repetisjoner - 1, 255, 255, 255);
+   ring.setPixelColor(repetisjoner - 1, hvit);
    ring.show();
  }
 
 void reset() {
   repetisjoner = 0;
   // Fjern LED lys
-  ring.fill(0, 0, repsPerOvelse);
+  ring.fill(0, 0, maksRepsPerOvelse);
   ring.show();
 }
 
@@ -158,24 +177,24 @@ void fullfortOvelse() {
   tone(pz,1319,850);
   delay(800);
   noTone(pz);
-  ring.setPixelColor(startPunktOvelseLED - brukerOgPoengMatrise[aktivBruker][aktivOvelse],255);
+  ring.setPixelColor(startPunktOvelseLED - brukerOgPoengMatrise[aktivBruker][aktivOvelse],blaa);
   ukentligPoeng[aktivBruker]++;
+  oppdaterVerdiIFil(aktivBrukerFilnavn(), ukentligPoeng[aktivBruker]);
   oppdaterUkentligLED();
 }
 
 void oppdaterOvelseLED(int poeng) {
-  //ring.fill(0, startPunktOvelseLED-2, 3);
   ring.setPixelColor(10, 0);
   ring.setPixelColor(9, 0);
   ring.setPixelColor(8, 0);
   if (poeng >= 1) {
-    ring.setPixelColor(10, 255);
+    ring.setPixelColor(10, blaa);
   }
   if (poeng >= 2) {
-    ring.setPixelColor(9, 255);
+    ring.setPixelColor(9, blaa);
   }
   if (poeng == 3) {
-    ring.setPixelColor(8, 255);
+    ring.setPixelColor(8, blaa);
   }
   ring.show();
 }
@@ -188,46 +207,23 @@ void oppdaterUkentligLED() {
   int aktivBrukerPoeng = ukentligPoeng[aktivBruker];
   int annenBrukerPoeng = ukentligPoeng[annenBruker];
   if (aktivBrukerPoeng == annenBrukerPoeng) {
-    ring.setPixelColor(7, 255, 255, 0);
-    ring.setPixelColor(11, 255, 255, 0);
+    ring.setPixelColor(7, gul);
+    ring.setPixelColor(11, gul);
   } else if (aktivBrukerPoeng > annenBrukerPoeng) {
-    ring.setPixelColor(7, 255, 0, 0);
-    ring.setPixelColor(11, 0, 255, 0);
+    ring.setPixelColor(7, rod);
+    ring.setPixelColor(11, gronn);
   } else {
-    ring.setPixelColor(7, 0, 255, 0);
-    ring.setPixelColor(11, 255, 0, 0);
+    ring.setPixelColor(7, gronn);
+    ring.setPixelColor(11, rod);
   }
-
-  /*
-  int poeng = ukentligPoeng[aktivBruker];
-  int annenBruker = hentAnnenBruker();
-  strip.clear();
-  Serial.println(poeng);
-  for (int i = 0; i < poeng; i++) {
-    int teller = 0;
-    int farge = strip.getPixelColor(teller);
-    if (farge + 42 > 255) {
-      teller++;
-      farge = strip.getPixelColor(teller);
-    }
-    if (ukentligPoeng[aktivBruker] >= ukentligPoeng[annenBruker]) {
-      strip.setPixelColor(teller, 0, 0, farge + 42);
-    } else {
-      strip.setPixelColor(teller, farge + 42, 0, 0);
-    }
-    
-  }
-  strip.show();
-  */
 }
 
 
 void lesOvelse() {
     potVal = analogRead(potPin);
     angle = map(potVal, 0, 1023, 0, 179);
-    //Serial.println(angle);
 
-  if (aktivOvelse != 0 && angle >= 0 && angle < 85) {
+  if (aktivOvelse != 0 && angle >= 10 && angle < 85) {
     
     Serial.println(angle);
     Serial.println("kom");
@@ -245,7 +241,7 @@ void lesOvelse() {
     oppdaterOvelseLED(brukerOgPoengMatrise[aktivBruker][aktivOvelse]);
   }
 
-  if (aktivOvelse != 2 && angle >= 100 && angle < 180) {
+  if (aktivOvelse != 2 && angle >= 105 && angle < 170) {
     
     Serial.println(angle);
     Serial.println("bli");
@@ -271,4 +267,35 @@ void hentUkentligPoeng() {
       ukentligPoeng[i] += brukerOgPoengMatrise[i][j];
     }
   }
+}
+
+void oppdaterVerdiIFil(String filN, int nyVerdi) {
+  File filen = SD.open(filN, FILE_WRITE);
+  filen.remove();
+  filen = SD.open(filN, FILE_WRITE);
+  filen.write(nyVerdi);
+  filen.close();
+}
+byte hentVerdiIFil(String filN) {
+  File filen = SD.open(filN);
+  byte verdi;
+  while (filen.available()) {
+    verdi += filen.read();
+  }
+  filen.close();
+  return verdi;
+}
+
+void SDKortoppsett() {
+    // Kobler opp SD-kort
+  if (!SD.begin(SD_CS_PIN)) {
+    Serial.println("Oppsett av SD-kort feilet.");
+    return;
+  }
+  Serial.println("Oppsett av SD-kort fullført.");
+}
+
+String aktivBrukerFilnavn() {
+  String filNavn = "bruker" + String(aktivBruker) + ".txt";
+  return filNavn;
 }
